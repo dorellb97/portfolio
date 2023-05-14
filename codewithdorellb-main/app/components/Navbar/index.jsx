e895aac7e12cbc02178769ea9645fbc2797df0a3
Dorell B <dorellb97@gmail.com>
2023-05-14 17:49:57 +0200

    about us mobile version

--- codewithdorellb-main/app/components/Navbar/index.jsx
+++ codewithdorellb-main/app/components/Navbar/index.jsx
@@ -79,6 +79,7 @@ export default function Navbar() {
       </div>
          <div className={auth ? `${styles.info_links_admin} ${styles.info_links}` : styles.info_links} >
          <Link prefetch={false} href="/#projects"><div>{t('header:projects')}</div></Link>
+         <Link prefetch={false} href="/#about_us"><div>{t('header:about_us')}</div></Link>
           <Link prefetch={false} href="https://bmc.link/codewithdorellb"><div>{t('header:support')}</div></Link>
           {auth &&
         <div style={{display: "flex", flexDirection: "column", gap: "1vw", marginTop: "2vw", marginBottom: "5vw"}}>
