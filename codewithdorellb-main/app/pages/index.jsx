f439585311a0c71e8517fb9d00c55717eb077cea
Dorell B <dorellb97@gmail.com>
2023-05-14 18:24:31 +0200

    logo png add

--- codewithdorellb-main/app/pages/index.jsx
+++ codewithdorellb-main/app/pages/index.jsx
@@ -16,7 +16,7 @@ export default function Hello() {
   return (
   <>
    <Head>
-   <link rel="icon" href="/logo.svg"/>
+   <link rel="icon" href="/logo.png"/>
         <title>Codewith_Dorell.B - Creating New Level Websites </title>
         <meta name="description" content="WebXwiz is a company specializing in creating websites of any type. We provide high SEO optimization, modern performance and high security." />
       </Head>
