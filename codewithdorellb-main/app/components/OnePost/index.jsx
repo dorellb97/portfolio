bdaf266e8317d47b8cf8fd2af1adc123fa396aa6
Dorell B <dorellb97@gmail.com>
2023-05-18 14:58:40 +0200

    reverese

--- codewithdorellb-main/app/components/OnePost/index.jsx
+++ codewithdorellb-main/app/components/OnePost/index.jsx
@@ -7,32 +7,13 @@ import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-const CodeComponent = ({ language, value }) => {
-  const [isCodeVisible, setCodeVisibility] = useState(false);
-
-  const toggleCodeVisibility = () => {
-    setCodeVisibility((prevVisibility) => !prevVisibility);
-  };
-
-  return (
-    <div className={styles.codeContainer}>
-      <pre className={isCodeVisible ? styles.expandedCode : styles.collapsedCode}>
-        {value}
-      </pre>
-      <button onClick={toggleCodeVisibility}>
-        {isCodeVisible ? "Show less" : "Show more"}
-      </button>
-    </div>
-  );
-};
-
 export default function OnePost({ queryId }) {
   console.log(queryId);
   const router = useRouter();
   const { data, loading, error } = useQuery(GET_ONE_POST, {
     variables: { getPostId: queryId },
   });
-  console.log(error);
+  console.log(error)
   const [creationDate, setCreationDate] = useState("");
 
   useEffect(() => {
@@ -53,6 +34,7 @@ export default function OnePost({ queryId }) {
 
     const date = new Date(parseInt(data?.getPost?.updatedAt));
     const year = date.getFullYear();
+    // const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const monthNumber = date.getMonth();
     const monthName = monthNames[monthNumber];
     const day = date.getDate().toString().padStart(2, "0");
@@ -66,25 +48,36 @@ export default function OnePost({ queryId }) {
           <p className={styles.date_text}>{`Published ${creationDate}`}</p>
           <p className={styles.title}>{data?.getPost?.title}</p>
 
-          <div className={styles.box}>
-            <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}>
-              <button>YouTube Channel</button>
-            </a>
-          </div>
+          {/* <div className={styles.box}>
+             <a href={`https://www.youtube.com/watch?v=${data?.getPost?.videoLink}`}><button> VIDEO HERE</button></a> 
+            
+          </div> */}
+           <div className={styles.box}>
+             <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}><button>YouTube Channel</button></a> 
+            
+          </div> 
 
-          <p className={styles.pretitle}>{data?.getPost?.pretitle}</p>
+          <p
+            className={styles.pretitle}
+          >{data?.getPost?.pretitle}</p>
         </div>
         <div className={styles.premark}>
           <ReactMarkdown
             className={styles.markdown}
             children={data?.getPost?.sourceCode}
-            remarkPlugins={[remarkGfm]}
-            components={{
-              code: CodeComponent,
-            }}
-          />
+            remarkPlugins={[remarkGfm]} 
+            
+              />
+          
+          
         </div>
+
       </div>
+      
     </div>
+    
+              
+            
+          
   );
 }
