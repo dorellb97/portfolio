e3d77244de39f15df49cebeaf85f7151f184386b
Dorell B <dorellb97@gmail.com>
2023-05-18 14:04:59 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/OnePost/index.jsx
+++ codewithdorellb-main/app/components/OnePost/index.jsx
@@ -1,40 +1,10 @@
 import { GET_ONE_POST } from "../../apollo/posts";
 import { useQuery } from "@apollo/client";
 import React, { useState, useEffect } from "react";
+import styles from "./Post.module.scss";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
-import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import darcula from "react-syntax-highlighter/dist/esm/styles/prism/darcula";
-import Collapse from "react-collapsible";
 import remarkGfm from "remark-gfm";
-import styles from "./Post.module.scss";
-
-const CodeBlock = ({ language, value }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleToggle = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Collapse trigger={language} open={expanded}>
-      <SyntaxHighlighter
-        language={language}
-        style={darcula}
-        lineNumberContainerStyle={{ display: "none" }}
-        wrapLines={true}
-        lineProps={{ style: { whiteSpace: "pre-wrap" } }}
-      >
-        {value}
-      </SyntaxHighlighter>
-      {!expanded && (
-        <div className={styles.expandButton} onClick={handleToggle}>
-          Show More
-        </div>
-      )}
-    </Collapse>
-  );
-};
 
 export default function OnePost({ queryId }) {
   const router = useRouter();
@@ -43,6 +13,7 @@ export default function OnePost({ queryId }) {
   });
 
   const [creationDate, setCreationDate] = useState("");
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     const monthNames = [
@@ -68,39 +39,35 @@ export default function OnePost({ queryId }) {
     setCreationDate(`${monthName} ${day}, ${year}`);
   }, [data]);
 
+  const handleToggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className={styles.preback}>
       <div className={styles.back}>
         <div className={styles.head}>
           <p className={styles.date_text}>{`Published ${creationDate}`}</p>
           <p className={styles.title}>{data?.getPost?.title}</p>
+
           <div className={styles.box}>
-            <a
-              href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}
-            >
+            <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}>
               <button>YouTube Channel</button>
             </a>
           </div>
+
           <p className={styles.pretitle}>{data?.getPost?.pretitle}</p>
         </div>
+
         <div className={styles.premark}>
           <ReactMarkdown
-            className={styles.markdown}
+            className={`${styles.markdown} ${expanded ? styles.expanded : ""}`}
             children={data?.getPost?.sourceCode}
             remarkPlugins={[remarkGfm]}
-            components={{
-              code({ node, inline, className, children, ...props }) {
-                const match = /language-(\w+)/.exec(className || "");
-                return !inline && match ? (
-                  <CodeBlock language={match[1]} value={String(children).replace(/\n$/, "")} {...props} />
-                ) : (
-                  <code className={className} {...props}>
-                    {children}
-                  </code>
-                );
-              },
-            }}
           />
+          <button className={styles.toggleButton} onClick={handleToggleExpand}>
+            {expanded ? "Collapse" : "Expand"}
+          </button>
         </div>
       </div>
     </div>
