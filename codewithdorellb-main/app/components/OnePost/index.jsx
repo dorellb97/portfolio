6a16bb33c8f0d211342b65d6dfe73abf88ea0f91
Dorell B <dorellb97@gmail.com>
2023-05-18 14:22:45 +0200

    reverse all

--- codewithdorellb-main/app/components/OnePost/index.jsx
+++ codewithdorellb-main/app/components/OnePost/index.jsx
@@ -1,20 +1,20 @@
 import { GET_ONE_POST } from "../../apollo/posts";
 import { useQuery } from "@apollo/client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./Post.module.scss";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Collapse } from "react-collapse";
 
 export default function OnePost({ queryId }) {
+  console.log(queryId);
   const router = useRouter();
   const { data, loading, error } = useQuery(GET_ONE_POST, {
     variables: { getPostId: queryId },
   });
-
+  console.log(error)
   const [creationDate, setCreationDate] = useState("");
-  const [collapsed, setCollapsed] = useState(true);
 
   useEffect(() => {
     const monthNames = [
@@ -34,30 +34,12 @@ export default function OnePost({ queryId }) {
 
     const date = new Date(parseInt(data?.getPost?.updatedAt));
     const year = date.getFullYear();
+    // const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const monthNumber = date.getMonth();
     const monthName = monthNames[monthNumber];
     const day = date.getDate().toString().padStart(2, "0");
     setCreationDate(`${monthName} ${day}, ${year}`);
   }, [data]);
-
-  const handleToggleCollapse = () => {
-    setCollapsed(!collapsed);
-  };
-
-  const renderers = {
-    code: ({ language, value }) => {
-      if (language && value) {
-        return (
-          <Collapse isOpened={!collapsed}>
-            <pre className={styles.codeBlock}>
-              <code>{value}</code>
-            </pre>
-          </Collapse>
-        );
-      }
-      return null;
-    },
-  };
 
   return (
     <div className={styles.preback}>
@@ -66,27 +48,36 @@ export default function OnePost({ queryId }) {
           <p className={styles.date_text}>{`Published ${creationDate}`}</p>
           <p className={styles.title}>{data?.getPost?.title}</p>
 
-          <div className={styles.box}>
-            <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}>
-              <button>YouTube Channel</button>
-            </a>
-          </div>
+          {/* <div className={styles.box}>
+             <a href={`https://www.youtube.com/watch?v=${data?.getPost?.videoLink}`}><button> VIDEO HERE</button></a> 
+            
+          </div> */}
+           <div className={styles.box}>
+             <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}><button>YouTube Channel</button></a> 
+            
+          </div> 
 
-          <p className={styles.pretitle}>{data?.getPost?.pretitle}</p>
+          <p
+            className={styles.pretitle}
+          >{data?.getPost?.pretitle}</p>
         </div>
-
         <div className={styles.premark}>
           <ReactMarkdown
             className={styles.markdown}
             children={data?.getPost?.sourceCode}
-            remarkPlugins={[remarkGfm]}
-            renderers={renderers}
-          />
-          <button className={styles.toggleButton} onClick={handleToggleCollapse}>
-            {collapsed ? "Expand" : "Collapse"}
-          </button>
+            remarkPlugins={[remarkGfm]} 
+            
+              />
+          
+          
         </div>
+
       </div>
+      
     </div>
+    
+              
+            
+          
   );
 }
