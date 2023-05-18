97ab8e0bfcfbac473952e35a60fb855a1271ce39
Dorell B <dorellb97@gmail.com>
2023-05-18 14:26:18 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/OnePost/index.jsx
+++ codewithdorellb-main/app/components/OnePost/index.jsx
@@ -1,19 +1,17 @@
 import { GET_ONE_POST } from "../../apollo/posts";
 import { useQuery } from "@apollo/client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Post.module.scss";
-import { useEffect } from "react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export default function OnePost({ queryId }) {
-  console.log(queryId);
   const router = useRouter();
   const { data, loading, error } = useQuery(GET_ONE_POST, {
     variables: { getPostId: queryId },
   });
-  console.log(error)
+
   const [creationDate, setCreationDate] = useState("");
 
   useEffect(() => {
@@ -34,12 +32,29 @@ export default function OnePost({ queryId }) {
 
     const date = new Date(parseInt(data?.getPost?.updatedAt));
     const year = date.getFullYear();
-    // const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const monthNumber = date.getMonth();
     const monthName = monthNames[monthNumber];
     const day = date.getDate().toString().padStart(2, "0");
     setCreationDate(`${monthName} ${day}, ${year}`);
   }, [data]);
+
+  const handleCopyCode = (code) => {
+    console.log(code);
+    // Copy the code to the console or perform any desired actions
+  };
+
+  const renderers = {
+    code: ({ language, value }) => {
+      if (language && value) {
+        return (
+          <pre className={styles.codeBlock}>
+            <code onClick={() => handleCopyCode(value)}>{value}</code>
+          </pre>
+        );
+      }
+      return null;
+    },
+  };
 
   return (
     <div className={styles.preback}>
@@ -48,36 +63,24 @@ export default function OnePost({ queryId }) {
           <p className={styles.date_text}>{`Published ${creationDate}`}</p>
           <p className={styles.title}>{data?.getPost?.title}</p>
 
-          {/* <div className={styles.box}>
-             <a href={`https://www.youtube.com/watch?v=${data?.getPost?.videoLink}`}><button> VIDEO HERE</button></a> 
-            
-          </div> */}
-           <div className={styles.box}>
-             <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}><button>YouTube Channel</button></a> 
-            
-          </div> 
+          <div className={styles.box}>
+            <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}>
+              <button>YouTube Channel</button>
+            </a>
+          </div>
 
-          <p
-            className={styles.pretitle}
-          >{data?.getPost?.pretitle}</p>
+          <p className={styles.pretitle}>{data?.getPost?.pretitle}</p>
         </div>
+
         <div className={styles.premark}>
           <ReactMarkdown
             className={styles.markdown}
             children={data?.getPost?.sourceCode}
-            remarkPlugins={[remarkGfm]} 
-            
-              />
-          
-          
+            remarkPlugins={[remarkGfm]}
+            renderers={renderers}
+          />
         </div>
-
       </div>
-      
     </div>
-    
-              
-            
-          
   );
 }
