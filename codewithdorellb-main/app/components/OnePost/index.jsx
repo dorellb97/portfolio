083ff94cb433fca43d4799ecababa1b65d576fe9
Dorell B <dorellb97@gmail.com>
2023-05-18 14:00:14 +0200

    reverse all

--- codewithdorellb-main/app/components/OnePost/index.jsx
+++ codewithdorellb-main/app/components/OnePost/index.jsx
@@ -1,54 +1,19 @@
 import { GET_ONE_POST } from "../../apollo/posts";
 import { useQuery } from "@apollo/client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import styles from "./Post.module.scss";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import Collapse from "react-collapsible";
 import remarkGfm from "remark-gfm";
-import styles from "./Post.module.scss";
-
-const CodeBlock = ({ language, value }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const handleToggle = () => {
-    setExpanded(!expanded);
-  };
-
-  return (
-    <Collapse trigger={language} open={expanded}>
-      <SyntaxHighlighter
-        language={language}
-        style={darcula}
-        showLineNumbers={!expanded}
-      >
-        {value}
-      </SyntaxHighlighter>
-      {!expanded && (
-        <div className={styles.expandButton} onClick={handleToggle}>
-          Show More
-        </div>
-      )}
-    </Collapse>
-  );
-};
-
-const renderers = {
-  code: ({ language, value }) => {
-    if (language && value) {
-      return <CodeBlock language={language} value={value} />;
-    }
-    return <pre>{value}</pre>;
-  },
-};
 
 export default function OnePost({ queryId }) {
+  console.log(queryId);
   const router = useRouter();
   const { data, loading, error } = useQuery(GET_ONE_POST, {
     variables: { getPostId: queryId },
   });
-
+  console.log(error)
   const [creationDate, setCreationDate] = useState("");
 
   useEffect(() => {
@@ -69,6 +34,7 @@ export default function OnePost({ queryId }) {
 
     const date = new Date(parseInt(data?.getPost?.updatedAt));
     const year = date.getFullYear();
+    // const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const monthNumber = date.getMonth();
     const monthName = monthNames[monthNumber];
     const day = date.getDate().toString().padStart(2, "0");
@@ -81,22 +47,37 @@ export default function OnePost({ queryId }) {
         <div className={styles.head}>
           <p className={styles.date_text}>{`Published ${creationDate}`}</p>
           <p className={styles.title}>{data?.getPost?.title}</p>
-          <div className={styles.box}>
-            <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}>
-              <button>YouTube Channel</button>
-            </a>
-          </div>
-          <p className={styles.pretitle}>{data?.getPost?.pretitle}</p>
+
+          {/* <div className={styles.box}>
+             <a href={`https://www.youtube.com/watch?v=${data?.getPost?.videoLink}`}><button> VIDEO HERE</button></a> 
+            
+          </div> */}
+           <div className={styles.box}>
+             <a href={`https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA`}><button>YouTube Channel</button></a> 
+            
+          </div> 
+
+          <p
+            className={styles.pretitle}
+          >{data?.getPost?.pretitle}</p>
         </div>
         <div className={styles.premark}>
           <ReactMarkdown
             className={styles.markdown}
             children={data?.getPost?.sourceCode}
-            remarkPlugins={[remarkGfm]}
-            components={renderers}
-          />
+            remarkPlugins={[remarkGfm]} 
+            
+              />
+          
+          
         </div>
+
       </div>
+      
     </div>
+    
+              
+            
+          
   );
 }
