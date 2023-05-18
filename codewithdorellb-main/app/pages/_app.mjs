5873d45f1c864ba59bd086a766aaa896d23c1682
Dorell B <dorellb97@gmail.com>
2023-05-18 12:59:19 +0200

    reverse all

--- codewithdorellb-main/app/pages/_app.mjs
+++ codewithdorellb-main/app/pages/_app.mjs
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as ReactRouter, Route, Switch } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client/react/context/ApolloProvider';
-import { createUploadLink } from 'apollo-upload-client';
-import { appWithTranslation } from 'next-i18next';
+import '../styles/globals.scss'
+import { InMemoryCache } from "@apollo/client/cache/inmemory/inMemoryCache.js";
+import { ApolloClient } from "@apollo/client/core/ApolloClient.js";
+import { ApolloProvider } from "@apollo/client/react/context/ApolloProvider.js";
+import { createUploadLink } from "apollo-upload-client";
+import Head from 'next/head'
+import Layout from "../components/Layout.jsx";
+import { AuthProvider } from "../hooks/AuthContext.jsx";
+import {  useState, useEffect } from "react";
+import { useRouter } from "next/router.js";
+import { appWithTranslation } from 'next-i18next'
 import { Provider } from 'react-redux';
-import NextRouter from 'next/router';
+import Router from 'next/router'
 import store from '../redux/store.jsx';
 import { ApolloCache } from '@apollo/client/core';
 import Loader from '../components/Loader/index.jsx';
 import Script from 'next/script';
 
-import { InMemoryCache } from '@apollo/client/cache/inmemory/inMemoryCache.js';
-import { ApolloClient } from '@apollo/client/core/ApolloClient.js';
-import { AuthProvider } from '../hooks/AuthContext.jsx';
-import Head from 'next/head';
-import Layout from '../components/Layout.jsx';
-import Disclaimer from './Disclaimer';
-
 const createApolloClient = (cache = {}) =>
   new ApolloClient({
-    ssrMode: typeof window === 'undefined',
+    ssrMode: typeof window === "undefined",
     cache: new InMemoryCache().restore(cache),
-    link: createUploadLink({ uri: process.env.API_URI, credentials: 'include' }),
+    link: createUploadLink({ uri: process.env.API_URI,
+    credentials: 'include'
+  }),
   });
-export const apolloClient = createApolloClient(ApolloCache);
+export const apolloClient = createApolloClient(ApolloCache)
 
-const App = ({ Component, pageProps }) => {
+const App = ({
+  Component,
+  pageProps
+}) => {
   const [loading, setLoading] = useState(false);
-  const router = NextRouter;
+  const router = useRouter();
 
   useEffect(() => {
-    router.events.on('routeChangeStart', () => {
+    router.events.on("routeChangeStart", () => {
       setLoading(true);
     });
-    router.events.on('routeChangeComplete', () => {
+    router.events.on("routeChangeComplete", () => {
       setLoading(false);
     });
   }, []);
@@ -47,46 +51,34 @@ const App = ({ Component, pageProps }) => {
   }
 
   if (typeof window === 'undefined') {
-    return <></>;
-  }
-
+    return <></>;}
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <ApolloProvider client={apolloClient}>
-          {loading ? (
-            <Loader />
-          ) : (
-            <Layout>
-              <Head></Head>
-              <main>
-                <>
-                  <Script
-                    id="Adsense-id"
-                    data-ad-client="ca-pub-6283396829393644"
-                    async
-                    strategy="afterInteractive"
-                    onError={(e) => {
-                      console.error('Script failed to load', e);
-                    }}
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6283396829393644"
-                  />
-                  <ReactRouter>
-                    <Switch>
-                      {/* Other routes for your application */}
-                      <Route path="/disclaimer" component={Disclaimer} />
-                      <Route path="/" exact component={Component} />
-                      {/* Add more routes as needed */}
-                    </Switch>
-                  </ReactRouter>
-                </>
-              </main>
-            </Layout>
-          )}
-        </ApolloProvider>
-      </AuthProvider>
-    </Provider>
-  );
-};
+  <AuthProvider>
+  <ApolloProvider client={apolloClient}>
+    {
+        loading ?<Loader />
+        :
+      <Layout>
+        <Head>
+        </Head>
+        <main>
+        <>
+    <Script id="Adsense-id" data-ad-client="ca-pub-6283396829393644"
+  async strategy="afterInteractive"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6283396829393644" 
+/>
+        <Component {...pageProps} />
+     
+        </>
+        </main>
+      </Layout>
+       }
+  </ApolloProvider>
+  </AuthProvider>
+  </Provider>
+  )
+}
 
 export default appWithTranslation(App);
