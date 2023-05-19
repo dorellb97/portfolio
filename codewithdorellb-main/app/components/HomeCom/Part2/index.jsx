d64ea82a6efc2a0e5cb7212f6d0f0a03398bbea0
Dorell B <dorellb97@gmail.com>
2023-05-19 13:05:44 +0200

    2

--- codewithdorellb-main/app/components/HomeCom/Part2/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/Part2/index.jsx
@@ -4,7 +4,7 @@ export default function Part2() {
   const {t} = useTranslation()
   return (
     <div className={styles.back} id="about-us">
-        <p className={styles.title}>{t('home:facts_title_1')}<span>{t('home:facts_title_2')}</span></p>
+        <p className={styles.title}>{t('home:facts_title_2')}</p>
         <div className={styles.ul}>
             <li>{t('home:facts_text_1')}</li>
             <li>{t('home:facts_text_2')}</li>
