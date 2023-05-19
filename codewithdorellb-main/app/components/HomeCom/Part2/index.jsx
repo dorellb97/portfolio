4a2fb67f7b65bde21934261c22d051405ce17337
Dorell B <dorellb97@gmail.com>
2023-05-19 13:23:32 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/HomeCom/Part2/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/Part2/index.jsx
@@ -7,13 +7,13 @@ export default function Part2() {
         <p className={styles.title}>{t('header:about_us')}</p>
         <div className={styles.ul}>
             <li>{t('home:facts_text_1')}</li>
-            <br> </br> 
+            
             <li>{t('home:facts_text_2')}</li>
-            <br> </br> 
+           
             <li>{t('home:facts_text_3')}</li>
-            <br> </br> 
+             
             <li>{t('home:facts_text_4')}</li>
-            <br> </br> 
+            
             <li>{t('home:facts_text_5')}</li>
         </div>
     </div>
