e277f3e810f7c8e09fc02ca64ab93d2bbcec6737
Dorell B <dorellb97@gmail.com>
2023-05-20 00:30:14 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/HomeCom/Part3/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/Part3/index.jsx
@@ -40,16 +40,16 @@ export default function Part3() {
       </div>
 
 
-      {/* <div className={styles.search} id='category'>
+       <div className={styles.search} id='category'>
       <label  for="projects">Choose a category</label>
       <select name="projects" id="cars">
         {filtered && filtered.map(obj => (
           <option value={obj.title} key={obj.id}>{obj.title}</option>
         ))}
       </select>
-      </div> */}
+      </div> 
 
-      
+
       <input type="search" placeholder="Search here..." value={searchText} onChange={(e) => setSearchText(e.target.value)} className={styles.search} />
 
       <div className={styles.back2}>
