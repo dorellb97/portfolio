bbe0b37b78c772654ab0a1e1fea7b8905f4c33c5
Dorell B <dorellb97@gmail.com>
2023-05-19 14:22:23 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/Footer/index.jsx
+++ codewithdorellb-main/app/components/Footer/index.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
         <div className={styles.last}>
         <a href='https://www.instagram.com/codewith_dorell.b/' style={{textDecoration: "none"}}><Image src="/instagram.svg" width={35} height={35}/></a>
         <a href='https://t.me/codewith_dorellb/' style={{textDecoration: "none"}}><Image src="/tg.svg" width={35} height={35}/></a>
-        <a href='https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA' style={{textDecoration: "none"}}><Image src="/youtube.svg" width={35} height={47}/></a>
+        <a href='https://www.youtube.com/channel/UCdJ38tbKf_VG8lHm1StjaUA' style={{textDecoration: "none"}}><Image src="/youtube.svg" width={35} height={35}/></a>
         </div>
     </div>
     )
