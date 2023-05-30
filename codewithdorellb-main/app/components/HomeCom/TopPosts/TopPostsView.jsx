300f165b2360a8156f0130b3c9a853870743d995
Dorell B <dorellb97@gmail.com>
2023-05-30 11:52:07 +0200

    Update TopPostsView.jsx

--- codewithdorellb-main/app/components/HomeCom/TopPosts/TopPostsView.jsx
+++ codewithdorellb-main/app/components/HomeCom/TopPosts/TopPostsView.jsx
@@ -67,7 +67,8 @@ const TopPostsView = ({ id, title, videoLink, sourceCode }) => {
                   height={56}
                   src={item?.videoLink}
                 />
-                <p className={styles.description}>{item.title}</p>
+                <p  href={`/projects/${id}`} className={styles.description}>{item.title}</p>
+                
               </div>
             );
           })}
