a95abe33b49f08b33f6940ebaadfa6d9bc82aee6
Dorell B <dorellb97@gmail.com>
2023-05-31 23:24:10 +0200

    reverse

--- codewithdorellb-main/app/components/HomeCom/Part3/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/Part3/index.jsx
@@ -107,9 +107,9 @@ export default function Part3() {
             />
           )
         )}
-        <div className={styles.glowingbtn}>
+        <div className={styles.box}>
           <Link href="/#home">
-            <button className={styles.glowingbtn}> Back to Header  </button>
+            <button className={styles.box}> Back to Header  </button>
           </Link>
         </div>
       </div>
