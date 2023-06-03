a10ea828c776f85903fa7eaaf6bb663860bbdcce
Dorell B <dorellb97@gmail.com>
2023-06-03 23:13:50 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/Navbar/index.jsx
+++ codewithdorellb-main/app/components/Navbar/index.jsx
@@ -191,7 +191,9 @@ function Navbar() {
           // <CgMenu className={styles.burger} onClick={() => setIsOpenMenu(!isOpenMenu)}/>  
         }
         </div>
+        <div>
         <iframe src= "http://www.profitabledisplaynetwork.com/fbfe14d99dd78af78f889fa9e7198342/invoke.js" scrolling="no" frameborder="0" height="90px" width="728px"></iframe> 
+        </div>
          <div id="ad-container" height="90px" width="728px"></div> 
       </div>
     )
