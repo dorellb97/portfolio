2f02b47727a49fe6fc30965bd2f028bac9f5536b
Dorell B <dorellb97@gmail.com>
2023-06-03 17:40:14 +0200

    ads

--- codewithdorellb-main/app/pages/_document.jsx
+++ codewithdorellb-main/app/pages/_document.jsx
@@ -7,6 +7,7 @@ export default function Document() {
         <meta name="keywords" content="web, development, site, website, web-creation,website creation, modern website, SEO, вебсайты, разработка, веб" />
         <meta name="author" content="Dorel Boencius" />
         <meta name="google-site-verification" content="F5p8KmxlsIE2HtKxI9-wL9AnENChWbYL0Ai0sX-Qckk" />
+        <meta name="clckd" content="10dcdb4fd357beceeaed3e11c0430453" />
 
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
