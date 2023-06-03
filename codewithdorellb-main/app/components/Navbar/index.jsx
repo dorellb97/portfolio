15886bad024783f2cb79691c816ca7fd79687720
Dorell B <dorellb97@gmail.com>
2023-06-03 17:58:44 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/Navbar/index.jsx
+++ codewithdorellb-main/app/components/Navbar/index.jsx
@@ -119,8 +119,8 @@ function Navbar() {
             selectedKeys={projectType}
             onSelectionChange={(e) => onSelect(e)}
           >
-           <a href="/projects">
-            <Dropdown.Item  key="Css">CSS Project</Dropdown.Item> </a>
+           
+            <Dropdown.Item href="/projects" key="Css">CSS Project</Dropdown.Item>
             <Dropdown.Item key="JS">JS Projects</Dropdown.Item>
             <Dropdown.Item key="Games">Games</Dropdown.Item>
         
