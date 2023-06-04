c5d99fe7883d64e33da8ab9a64c503bc34875754
Dorell B <dorellb97@gmail.com>
2023-06-04 15:51:28 +0200

    upd

--- codewithdorellb-main/app/pages/about-us.jsx
+++ codewithdorellb-main/app/pages/about-us.jsx
@@ -12,7 +12,6 @@ export async function getStaticProps({ locale }) {
 export default function Aboutus() {
   return (
     <div style={{width: "100vw", display: "flex", justifyContent: "center", paddingTop: "3vw", height: "100vh"}}>
-     <iframe src="//cavalryconvincing.com/watchnew?key=fbfe14d99dd78af78f889fa9e7198342"  align-items="center" frameborder="0" scrolling="no"></iframe>
         <Part2 />
     </div>
   )
