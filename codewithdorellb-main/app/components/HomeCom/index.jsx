ec473d895476c6f218109b0606ca4113a2bfba9f
Dorell B <dorellb97@gmail.com>
2023-06-04 15:18:21 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/HomeCom/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/index.jsx
@@ -22,7 +22,15 @@ export default function HomeCom() {
         <div className={styles.hideWeb}>
           <TopPostsView />
           
-          <div>
+          <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100vw",
+          gap: "100px",
+        }}
+      >
           <iframe src="//cavalryconvincing.com/watchnew?key=57372e2a84c5b521c3b130c6d039f938" height ="300"width ="160" align-items="center" frameborder="0" scrolling="no"></iframe>
      </div>
      </div>
@@ -34,7 +42,15 @@ export default function HomeCom() {
       <div className={styles.hideTab}>
         <TopPostsView />
        
-        <div>
+        <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100vw",
+          gap: "100px",
+        }}
+      >
          <iframe src="//cavalryconvincing.com/watchnew?key=57372e2a84c5b521c3b130c6d039f938" height ="300"width ="160" align-items="center" frameborder="0" scrolling="no"></iframe>  
          </div>
       </div>
