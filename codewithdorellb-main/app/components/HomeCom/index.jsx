da362ff5f188f8c5c30f28df4c6ad8ba6d0507fd
Dorell B <dorellb97@gmail.com>
2023-06-04 14:16:22 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/HomeCom/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/index.jsx
@@ -21,7 +21,7 @@ export default function HomeCom() {
         <Part1 />
         <div className={styles.hideWeb}>
           <TopPostsView />
-          <iframe src="//cavalryconvincing.com/d826116ece53ad17fb6e20679b7522d4" width="300" height="250" frameborder="0" scrolling="no"></iframe>
+          <iframe src="//cavalryconvincing.com/d826116ece53ad17fb6e20679b7522d4/invoke.js" width="300" height="250" frameborder="0" scrolling="no"></iframe>
         </div>
         <Part3 />
         {/* <TestComponent /> */}
