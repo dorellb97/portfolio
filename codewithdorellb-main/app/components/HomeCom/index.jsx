cce23d4c357e2a07f1c34bcce56e5ebfb849c0b8
Dorell B <dorellb97@gmail.com>
2023-06-04 16:04:03 +0200

    Update index.jsx

--- codewithdorellb-main/app/components/HomeCom/index.jsx
+++ codewithdorellb-main/app/components/HomeCom/index.jsx
@@ -32,10 +32,11 @@ export default function HomeCom() {
         }}
       >
           <iframe src="//cavalryconvincing.com/watchnew?key=57372e2a84c5b521c3b130c6d039f938" height ="300"width ="160" align-items="center" frameborder="0" scrolling="no"></iframe>
+           </div>
           <div>
-          <iframe src="//cavalryconvincing.com/watchnew?key=9281de430afa613004176a26d7ff887c" width="300" height="250" frameborder="0" scrolling="no"></iframe>
+          <iframe src="//cavalryconvincing.com/watchnew?key=9281de430afa613004176a26d7ff887c" width="350" height="50" frameborder="0" scrolling="no"></iframe>
      </div>
-     </div>
+    
      </div>
         
         <Part3 />
@@ -47,7 +48,7 @@ export default function HomeCom() {
         <div>
          <iframe src="//cavalryconvincing.com/watchnew?key=57372e2a84c5b521c3b130c6d039f938" height ="300"width ="160" align-items="center" frameborder="0" scrolling="no"></iframe>  
          <div>
-         <iframe src="//cavalryconvincing.com/watchnew?key=9281de430afa613004176a26d7ff887c" width="300" height="250" frameborder="0" scrolling="no"></iframe>
+         <iframe src="//cavalryconvincing.com/watchnew?key=9281de430afa613004176a26d7ff887c" width="350" height="50" frameborder="0" scrolling="no"></iframe>
          </div>
          </div>
       </div>
